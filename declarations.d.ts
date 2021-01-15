d15dbb1635efb540fc14ac5369fe95eddba9ef0a
m0nch1 <m0nch1.develop@gmail.com>
2021-01-15 16:56:09 +0900

    fetch items and show index

---
+++ declarations.d.ts
@@ -0,0 +1,6 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    readonly SHOPIFY_DOMAIN: string;
+    readonly STORE_FRONT_ACCESS_TOKEN: string;
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,39 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Client, { Product } from "shopify-buy"
+import { GetStaticProps } from "next"
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+type IndexProps = {
+  products: Product[];
+};
 
+const client = Client.buildClient({
+  domain: process.env.SHOPIFY_DOMAIN,
+  storefrontAccessToken: process.env.STORE_FRONT_ACCESS_TOKEN,
+  language: 'ja-JP'
+})
+
+const IndexPage: React.FC<IndexProps> = ({ products }) => {
+  return (
+    <>
+      <h1>Hello Next.js 👋</h1>
+      <ul>
+        {
+          products.map((product) =>
+            <li key={product.id}>
+              {product.title}
+              <img src={product.images[0].src} height={80} />
+            </li>)
+        }
+      </ul>
+    </>
+  );
+}
 export default IndexPage
+
+export const getStaticProps: GetStaticProps = async () => {
+  const products: any = await client.product.fetchAll();
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products)),
+    },
+  };
+};
