b1cfb0f43bda46ff477d986f88f738ba3faf4579
m0nch1 <m0nch1.develop@gmail.com>
2021-01-22 23:44:23 +0900

    show product price

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -24,6 +24,7 @@ const IndexPage: React.FC<IndexProps> = ({ products }) => {
             //   </Link>
             // </li>
             <div key={product.id} className="w-screen h-screen flex justify-center items-center">
+              {console.log(product)}
               <div className="container mx-auto max-w-sm w-full p-4 sm:w-1/2">
                 <div className="card flex flex-col justify-center p-10 bg-white rounded-lg shadow-2xl">
                   <div className="prod-title">
@@ -38,7 +39,7 @@ const IndexPage: React.FC<IndexProps> = ({ products }) => {
                   </div>
                   <div className="prod-info grid gap-10">
                     <div className="flex flex-col md:flex-row justify-between items-center text-gray-900">
-                      <p className="font-bold text-xl">65 $</p>
+                      <p className="font-bold text-xl">{product.variants[0].price}円</p>
                       <Link href="/products/[id]" as={`/products/${product.id}`}>
                         <button
                           className="px-6 py-2 transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none">
