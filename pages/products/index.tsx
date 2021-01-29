09e47ebf1202e04f4ec6f53aa3ce90a40d77d8a2
m0nch1 <m0nch1.develop@gmail.com>
2021-01-29 20:16:43 +0900

    fix

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -34,7 +34,7 @@ const IndexPage: React.FC<IndexProps> = ({ products }) => {
                     <Link href="/products/[id]" as={`/products/${product.id}`}>
                       <button
                         className="px-6 py-2 transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none">
-                        Add to cart
+                        More Detail
                       </button>
                     </Link>
                   </div>
