c6efdaea572c13b963d7f9733c9d74aaf87b7e96
m0nch1 <m0nch1.develop@gmail.com>
2021-02-08 23:59:59 +0900

    delete comment

--- components/Molecules/ItemCard.tsx
+++ components/Molecules/ItemCard.tsx
@@ -6,8 +6,6 @@ type Props = {
   ItemPrice: string,
 }
 
-//'https://images.unsplash.com/photo-1563170351-be82bc888aa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=376&q=80'
-
 const ItemCard = ({ backgroundImageUrl, ItemName, ItemPrice }: Props) => (
   <div className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
     <div className="flex items-end justify-end h-56 w-full bg-cover" style={{
