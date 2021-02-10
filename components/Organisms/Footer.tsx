f8837728606d6ed0fa86bf31cde034a83941e981
m0nch1 <m0nch1.develop@gmail.com>
2021-02-10 19:00:42 +0900

    update footer and some fix

--- components/Organisms/Footer.tsx
+++ components/Organisms/Footer.tsx
@@ -1,7 +1,14 @@
 const Footer = () => (
-  <footer>
-    <hr />
-    <span>Footer</span>
+  <footer className="footer bg-white relative pt-1 border-b-2 border-gray-700">
+    <div className="container mx-auto px-6">
+      <div className="mt-16 border-t-2 border-gray-300 flex flex-col items-center">
+        <div className="sm:w-2/3 text-center py-6">
+          <p className="text-sm text-gray-700 font-bold mb-2">
+            @ 2021 by sample e-commerce
+          </p>
+        </div>
+      </div>
+    </div>
   </footer>
 )
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -59,16 +59,6 @@ const Home = () => (
         </div>
       </div>
 
-      <div className="mt-16">
-        <h3 className="text-gray-600 text-2xl font-medium">Fashions</h3>
-        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
-          <ItemCard backgroundImageUrl='https://images.unsplash.com/photo-1563170351-be82bc888aa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=376&q=80' ItemName="Chanel" ItemPrice="$12" />
-          <ItemCard backgroundImageUrl='https://images.unsplash.com/photo-1544441893-675973e31985?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80' ItemName="Man Mix" ItemPrice="$12" />
-          <ItemCard backgroundImageUrl='https://images.unsplash.com/photo-1532667449560-72a95c8d381b?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80' ItemName="Classic watch" ItemPrice="$12" />
-          <ItemCard backgroundImageUrl='https://images.unsplash.com/photo-1590664863685-a99ef05e9f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=345&q=80' ItemName="woman mix" ItemPrice="$12" />
-        </div>
-      </div>
-
     </div>
 
   </Layout >
