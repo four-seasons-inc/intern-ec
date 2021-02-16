9228e34f18a41f7be48b9db00ea3a0db9355e813
m0nch1 <m0nch1.develop@gmail.com>
2021-02-17 02:29:51 +0900

    update ItemCard

--- components/Molecules/ItemCard.tsx
+++ components/Molecules/ItemCard.tsx
@@ -8,14 +8,16 @@ type Props = {
 
 const ItemCard = ({ backgroundImageUrl, ItemName, ItemPrice }: Props) => (
   <div className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
-    <div className="flex items-end justify-end h-56 w-full bg-cover" style={{
-      backgroundImage: `url(${backgroundImageUrl})`
-    }}>
-      <AddCartButton />
+    <div className="prod-img">
+      <img src={backgroundImageUrl}
+        className="object-cover object-center h-56 w-full bg-cover" />
     </div>
-    <div className="px-5 py-3">
-      <h3 className="text-gray-700 uppercase">{ItemName}</h3>
-      <span className="text-gray-500 mt-2">{ItemPrice}</span>
+    <div className="flex items-end justify-between px-5 py-3">
+      <div>
+        <h3 className="text-gray-700 uppercase">{ItemName}</h3>
+        <span className="text-gray-500 mt-2">{ItemPrice}</span>
+      </div>
+      <AddCartButton />
     </div>
   </div>
 )
