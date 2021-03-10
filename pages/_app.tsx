5766d471aab1fcaf36bc58c101c2620149d6f9aa
Sota0403 <manage.kume@gmail.com>
2021-03-10 11:18:25 +0900

    delete 不要記述削除 && fix faviconの表示

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '@assets/main.css'
 
 import { AppProps } from 'next/app';
 
-import Head from '@components/Organisms/Head.tsx'
+import Head from '@components/Organisms/Head'
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
